8135c2fa0377f7eb033d0aaee4fdf6549fc8d235
PalNik <paha-87@mail.ru>
2025-07-09 21:50:34 +1000

    Added function getRandomNumRange in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+// Возвращает положительное число
+const getPositiveNumber = (number) => {
+  number = Number(number);
+  return (number < 0) ? (number * (-1)) : number;
+}
+
+// Возвращает случайное целое положительное число из диапазона
+const getRandomNumRange = (firstNum, secondNum) => {
+  firstNum = getPositiveNumber(firstNum);
+  secondNum = getPositiveNumber(secondNum);
+  const min = Math.min(firstNum, secondNum);
+  const max = Math.max(firstNum, secondNum);
+  return  Math.floor(Math.random() * (max - min + 1)) + min;
+}
