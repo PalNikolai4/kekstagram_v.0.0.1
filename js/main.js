bdc31eb9d1a0faab084f47cfbad0d84f0437310e
PalNik <paha-87@mail.ru>
2025-07-10 08:41:09 +1000

    Completed task 2.15. Added 2 functions - getRandomNumRange, isStringLessNum

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const getPositiveNumber = (number) => {
   number = Number(number);
   return (number < 0) ? (number * (-1)) : number;
-}
+};
 
 // Возвращает случайное целое положительное число из диапазона
 const getRandomNumRange = (firstNum, secondNum) => {
@@ -11,4 +11,9 @@ const getRandomNumRange = (firstNum, secondNum) => {
   const min = Math.min(firstNum, secondNum);
   const max = Math.max(firstNum, secondNum);
   return  Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+getRandomNumRange(1, 5);
+
+// Сравнивает длину строки с числом.
+const isStringLessNum = (string, maxLength) => Number(string.length) < Number(maxLength);
+isStringLessNum('afkj oijadf ojoj', 2);
