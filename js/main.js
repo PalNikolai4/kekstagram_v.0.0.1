1d6ae87cc06250109c76159bf34799852f810d68
PalNik <paha-87@mail.ru>
2025-07-12 19:47:52 +1000

    Made functions: getArrayUniqueNumbers, getPhotoDescription

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// 'use strict';
+
+// Сравнивает длину строки с числом.
+const checkStringLength = (string = '', maxLength = 0) => Number(string.length) < Number(maxLength);
+checkStringLength();
+
 // Возвращает положительное число
 const getPositiveNumber = (number) => {
   number = Number(number);
@@ -5,15 +11,49 @@ const getPositiveNumber = (number) => {
 };
 
 // Возвращает случайное целое положительное число из диапазона
-const getRandomNumRange = (firstNum = 0, secondNum = 100) => {
+const getRandomNumRange = (firstNum = 0, lastNum = 100) => {
   firstNum = getPositiveNumber(firstNum);
-  secondNum = getPositiveNumber(secondNum);
-  const min = Math.min(firstNum, secondNum);
-  const max = Math.max(firstNum, secondNum);
+  lastNum = getPositiveNumber(lastNum);
+  const min = Math.min(firstNum, lastNum);
+  const max = Math.max(firstNum, lastNum);
   return  Math.floor(Math.random() * (max - min + 1)) + min;
 };
 getRandomNumRange();
 
-// Сравнивает длину строки с числом.
-const checkStringLength = (string = "", maxLength = 0) => Number(string.length) < Number(maxLength);
-checkStringLength();
+// Возвращает уникальное случайное целое положительное число из диапазона
+const getArrayUniqueNumbers = (firstNum = 0, lastNum = 100, countNumbers = 10) => {
+  // Проверка на возможность получения количества уникальных чисел из заданного диапазона
+  // Проверка не обязательна. Можно удалить
+  if (countNumbers > (lastNum - firstNum + 1)) {
+    firstNum = getPositiveNumber(firstNum);
+    lastNum = getPositiveNumber(lastNum);
+    let countNumberFromRange = lastNum - firstNum + 1;
+    let difference = countNumbers - countNumberFromRange;
+    message = `Количество запрошенных уникальных чисел (${countNumbers}) больше диапазона возможных чисел (${countNumberFromRange}) на ${difference}`;
+    return console.log(message);
+  }
+
+  // Получение массива случайных чисел
+  const uniqueNumbers = [];
+  while (uniqueNumbers.length < countNumbers) {
+    let num = getRandomNumRange(firstNum, lastNum);
+    if (!uniqueNumbers.includes(num)) {
+      uniqueNumbers.push(num);
+    }
+  }
+  return uniqueNumbers;
+};
+getArrayUniqueNumbers();
+
+
+//
+const getPhotoDescription = () => {
+  return {
+    id: 1,
+    url: `photos/1.jpg`,
+    description: 'Description photo',
+    likes: getRandomNumRange(15, 200),
+    comments: []
+  }
+}
+console.log(getPhotoDescription());
