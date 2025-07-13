977c06ae19637a7cd75181a8d8615e2c0a113b76
PalNik <paha-87@mail.ru>
2025-07-13 13:02:52 +1000

    Completed task 4.7. Added functions: getComment, getComments, getPhotoDescription, getPhotosDescriptions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// 'use strict';
+const NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const TEXT_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const PHOTO_DESCRIPTIONS = ['Сказочное Бали', 'Фото из 2020', 'Славное было время', 'Немного эстетики', 'Are you ready?', 'Вы этому верите?!', 'Пусть будет тута', 'А что бы написал ты?', 'Продолжение следует'];
 
 // Сравнивает длину строки с числом.
 const checkStringLength = (string = '', maxLength = 0) => Number(string.length) < Number(maxLength);
@@ -11,7 +13,7 @@ const getPositiveNumber = (number) => {
 };
 
 // Возвращает случайное целое положительное число из диапазона
-const getRandomNumRange = (firstNum = 0, lastNum = 100) => {
+const getRandomNumRange = (firstNum = 1, lastNum = 10) => {
   firstNum = getPositiveNumber(firstNum);
   lastNum = getPositiveNumber(lastNum);
   const min = Math.min(firstNum, lastNum);
@@ -20,20 +22,8 @@ const getRandomNumRange = (firstNum = 0, lastNum = 100) => {
 };
 getRandomNumRange();
 
-// Возвращает уникальное случайное целое положительное число из диапазона
-const getArrayUniqueNumbers = (firstNum = 0, lastNum = 100, countNumbers = 10) => {
-  // Проверка на возможность получения количества уникальных чисел из заданного диапазона
-  // Проверка не обязательна. Можно удалить
-  if (countNumbers > (lastNum - firstNum + 1)) {
-    firstNum = getPositiveNumber(firstNum);
-    lastNum = getPositiveNumber(lastNum);
-    let countNumberFromRange = lastNum - firstNum + 1;
-    let difference = countNumbers - countNumberFromRange;
-    message = `Количество запрошенных уникальных чисел (${countNumbers}) больше диапазона возможных чисел (${countNumberFromRange}) на ${difference}`;
-    return console.log(message);
-  }
-
-  // Получение массива случайных чисел
+// Возвращает массив уникальных случайных целых положительных чисел из диапазона
+const getArrayUniqueNumbers = (firstNum = 1, lastNum = 10, countNumbers = (lastNum - firstNum + 1)) => {
   const uniqueNumbers = [];
   while (uniqueNumbers.length < countNumbers) {
     let num = getRandomNumRange(firstNum, lastNum);
@@ -45,15 +35,58 @@ const getArrayUniqueNumbers = (firstNum = 0, lastNum = 100, countNumbers = 10) =
 };
 getArrayUniqueNumbers();
 
-
-//
-const getPhotoDescription = () => {
+// Возвращает комментарий к фотографии
+const getComment = () => {
+  const message = TEXT_COMMENTS[getRandomNumRange(0, TEXT_COMMENTS.length - 1)];
+  const name = NAMES[getRandomNumRange(0, NAMES.length - 1)];
+  const id = getRandomNumRange(1, 1000);
   return {
-    id: 1,
-    url: `photos/1.jpg`,
-    description: 'Description photo',
-    likes: getRandomNumRange(15, 200),
-    comments: []
+    id: id,
+    avatar: `img/avatar-${id}.svg`,
+    message: message,
+    name: name
   }
 }
-console.log(getPhotoDescription());
+
+// Возвращает массив комментариев к фотографии
+const getComments = (countComments) => {
+  if (!countComments) {
+    countComments = getRandomNumRange(1, 10);
+  }
+  const comments = [];
+  while (comments.length < countComments) {
+    comments.push(getComment());
+  }
+  return comments;
+}
+
+// Возвращает описание и комментарии к фотографии
+const getPhotoDescription = (id) => {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: PHOTO_DESCRIPTIONS[getRandomNumRange(0, (PHOTO_DESCRIPTIONS.length - 1))],
+    likes: getRandomNumRange(15, 200),
+    comments: getComments()
+  }
+}
+getPhotoDescription();
+
+// Возвращает массив описаний и комментариев к фотографиям
+const getPhotosDescriptions = (countDescriptions) => {
+  if (!countDescriptions || countDescriptions > 25) {
+    countDescriptions = getRandomNumRange(1, 25);
+  }
+
+  const descriptionsArr = [];
+  const idArr = getArrayUniqueNumbers(1, countDescriptions);
+
+  for (let i = 0; i < countDescriptions; i++) {
+    const description = getPhotoDescription(idArr[i]);
+    descriptionsArr.push(description);
+  }
+
+  return descriptionsArr;
+}
+const descriptionsArr = getPhotosDescriptions();
+console.log(descriptionsArr);
