de9f1be2be1ccddeec7bf5da772ccc7d8ce54e7e
PalNik <paha-87@mail.ru>
2025-07-30 09:42:59 +1000

    Added form.js

--- js/picture.js
+++ js/picture.js
@@ -5,25 +5,19 @@ const picturesContainer = document.querySelector('.pictures');
 
 // Копирует и  заполняет шаблон #picture данными. Возвращает заполненный шаблон 1 фотографии
 const renderPicture = (data) => {
-  const { url, description, likes, comments} = data;
+  const { url, description, likes, comments } = data;
   const picture = pictureTemplate.cloneNode(true);
   picture.querySelector('.picture__img').src = url;
   picture.querySelector('.picture__img').alt = description;
   picture.querySelector('.picture__comments').textContent = comments.length;
   picture.querySelector('.picture__likes').textContent = likes;
+
   // Этот слушатель вынести отсюда и добавить его на родителя
   picture.addEventListener('click', () => {
     openFullPicture(data);
   });
   return picture;
 }
-
-// const onShowFullPicture = (evt) => {
-//   evt.preventDefault();
-//   if (evt.target === ) {
-//     openFullPicture(data);
-//   }
-// }
 
 // Создаёт фрагмент-хранилище. Создаёт шаблоны по количеству данных и заполняет их этими данными. Возвращает хранилище с заполненными шаблонами
 const renderPictures = (elements) => {
@@ -35,5 +29,6 @@ const renderPictures = (elements) => {
 
   picturesContainer.append(picturesFragment);
 }
+
 
 export { renderPictures }
