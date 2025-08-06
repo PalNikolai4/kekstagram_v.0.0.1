892e6f26b0f3db1802c7773386296812796b1262
PalNik <paha-87@mail.ru>
2025-08-06 10:38:20 +1000

    Implemented the logic of opening a upload-modal and closing it: when clicking on closeButton, when the ESC keydown, when clicking on the overlay

--- js/modal-upload.js
+++ js/modal-upload.js
@@ -1,40 +1,45 @@
+import { isEsc } from './utill.js';
+import { showSelectedPhoto } from './edit-upload.js';
+
 const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
-const uploadField = form.querySelector('#upload-file');
 const imgUploadOverlay = form.querySelector('.img-upload__overlay');
-const imgUploadPreview = form.querySelector('.img-upload__preview').querySelector('img');
+const uploadField = form.querySelector('#upload-file');
+const closeButton = form.querySelector('.img-upload__cancel');
+const uploadOverlay = form.querySelector('.img-upload__overlay');
 
-// Выбор фотографии и её подстановка в окно предпросмотра
-const onPhotoEditingModal = (isClose) => {
-  const file = uploadField.files[0];
-  if (!file) return;
-  if (!isClose) {
-    imgUploadPreview.src = URL.createObjectURL(file);
-  } else {
-    URL.revokeObjectURL(file);
-  }
-}
-// addEventListener on uploadField or form ???
 const openPhotoEditingModal = () => {
   uploadField.addEventListener('change', () => {
     body.classList.add('modal-open');
     imgUploadOverlay.classList.remove('hidden');
 
-    onPhotoEditingModal();
+    showSelectedPhoto();
+    document.addEventListener('keydown', onEscKeyDown);
+    closeButton.addEventListener('click', closePhotoEditingModal);
+    uploadOverlay.addEventListener('click', onUploadOverlayClick);
     })
 }
+
 const closePhotoEditingModal = () => {
     body.classList.remove('modal-open');
     imgUploadOverlay.classList.add('hidden');
 
-    onPhotoEditingModal(true);
-
+    document.removeEventListener('keydown', onEscKeyDown);
+    closeButton.removeEventListener('click', closePhotoEditingModal);
+    uploadOverlay.removeEventListener('click', onUploadOverlayClick);
+    showSelectedPhoto(true);
 }
 
+const onEscKeyDown = (evt) => {
+  if (isEsc(evt)) {
+    closePhotoEditingModal();
+  }
+}
 
-
-
-
-
+const onUploadOverlayClick = (evt) => {
+  if (evt.target === uploadOverlay) {
+    closePhotoEditingModal();
+  }
+}
 
 export { openPhotoEditingModal }
