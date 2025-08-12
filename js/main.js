4d957d444e8001a154e03fe75ec844fbb0518a97
PalNik <paha-87@mail.ru>
2025-08-12 20:15:08 +1000

    Create function - createdEffectsSlider & connect to main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import { getPictures } from './data.js';
 import { renderPictures, renderInfoFullPicture } from './picture.js';
 import { openPhotoEditingModal } from './modal-upload.js';
+import { createdEffectsSlider } from './slider-upload.js';
 
 const picturesData = getPictures(25);
 renderPictures(picturesData);
 renderInfoFullPicture(picturesData);
 openPhotoEditingModal();
+createdEffectsSlider();
