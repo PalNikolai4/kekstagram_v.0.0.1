0f9090e2e4562d7a265def9ed3620dd3c1b7686f
PalNik <paha-87@mail.ru>
2025-08-23 13:17:56 +1000

    Removed the full-picture-hashtags.js to validate-upload-form.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { getPictures } from './data.js';
 import { renderPictures, renderInfoFullPicture } from './picture.js';
 import { openPhotoEditingModal } from './modal-upload.js';
-import './full-picture-hashtags.js';
+import './validate-upload-form.js';
 
 const picturesData = getPictures(25);
 renderPictures(picturesData);
