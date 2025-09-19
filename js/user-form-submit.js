0c72a104489d839e9aab62897fd00cd34a70ffc5
PalNik <paha-87@mail.ru>
2025-09-19 18:07:12 +1000

    User form submit result message
    - Added deletion of message when clicking a button.

--- js/user-form-submit.js
+++ js/user-form-submit.js
@@ -1,13 +1,18 @@
 const showMessageFormSubmissionResult = (isSuccess) => {
   let classElement = null;
   isSuccess ? classElement = 'success' : classElement = 'error';
-  const messageElement = document
-  .querySelector(`#${classElement}`)
-  .content
-  .querySelector(`.${classElement}`);
+
+  const template = document.querySelector(`#${classElement}`);
+  const messageElement = template.content.querySelector(`.${classElement}`).cloneNode(true);
 
   const body = document.querySelector('body');
   body.insertAdjacentElement('beforeend', messageElement);
+
+  const button = messageElement.querySelector(`.${classElement}__button`);
+  button.addEventListener('click', () => {
+    messageElement.remove();
+  })
+
 };
 
 export { showMessageFormSubmissionResult }
