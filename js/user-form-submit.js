4c4f4f65a1b22c3861b8d39a2e8acfd1cdcb3ed6
PalNik <paha-87@mail.ru>
2025-09-22 18:51:33 +1000

    User form submit result message
    Added to function showMessageFormSubmissionResult closing the message box when:
    - clicking on the overlay;
    - pressing button 2.

--- js/user-form-submit.js
+++ js/user-form-submit.js
@@ -1,3 +1,5 @@
+import { isEsc } from "./utill.js";
+
 const showMessageFormSubmissionResult = (isSuccess) => {
   let classElement = null;
   isSuccess ? classElement = 'success' : classElement = 'error';
@@ -8,10 +10,26 @@ const showMessageFormSubmissionResult = (isSuccess) => {
   const body = document.querySelector('body');
   body.insertAdjacentElement('beforeend', messageElement);
 
+  const onEscKeyDown = (evt) => {
+    if (isEsc(evt)) {
+      messageElement.remove();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+  document.addEventListener('keydown', onEscKeyDown);
+
   const button = messageElement.querySelector(`.${classElement}__button`);
   button.addEventListener('click', () => {
     messageElement.remove();
-  })
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  messageElement.addEventListener('click', (evt) => {
+    if (evt.target === messageElement) {
+      messageElement.remove();
+      document.removeEventListener('keydown', onEscKeyDown);
+    };
+  });
 
 };
 
